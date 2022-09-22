cbb340d68681f88be7422c624b42421935fc90ea
Kevin <maskevin.w@gmail.com>
2022-09-22 18:56:42 +0200

    Creating signup Form and logic of form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,13 +4,13 @@ import {
     signInWithRedirect,
     signInWithPopup,
     GoogleAuthProvider,
+    createUserWithEmailAndPassword
 } from 'firebase/auth'
 import { 
     getFirestore, 
     doc,
     setDoc,
     getDoc,
-    connectFirestoreEmulator,
  } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -25,17 +25,20 @@ const firebaseConfig = {
   // Initialize Firebase
   const firebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
+  const googleProvider = new GoogleAuthProvider();
+  googleProvider.setCustomParameters({
     prompt: "select_account"
   });
 
   export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+  export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
   export const db = getFirestore();
 
   export const createUserDocumentFromAuth = async (userAuth) => {
+    if(!userAuth) return;
+    
     const userDocRef = doc(db, 'users', userAuth.uid)
 
     const userSnapshot = await getDoc(userDocRef);
@@ -54,6 +57,12 @@ const firebaseConfig = {
             console.log('error creating the user', error.message)
         }
     }
-
+    
     return userDocRef;
+ }
+
+ export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await createAuthUserWithEmailAndPassword(auth, email, password)
  }
