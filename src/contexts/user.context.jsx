3d05f09628ddb3cb8a2b2c24c4f0ef974199fcb8
Kevin Mas <maskevin.w@gmail.com>
2022-10-01 16:00:02 +0200

    adding cart and toggle view

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -18,7 +18,9 @@ export const UserProvider = ({children}) => {
             }
             setCurrentUser(user)
 
-        })
+        });
+
+        return unsubscribe;
     }, [])
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,19 @@
 import { Link, Outlet } from 'react-router-dom'
-import { Fragment, useContext } from 'react'
+import { Fragment, useContext, useState } from 'react'
 
 import './navigation.styles.scss'
 
 import logo from '../../assets/crown.svg';
 import { UserContext } from "../../contexts/user.context";
+import {CartContext} from "../../contexts/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext)
+    const { isCartOpen } = useContext(CartContext)
 
   return (
     <Fragment>
@@ -22,7 +26,9 @@ const Navigation = () => {
           </Link>
             {currentUser ? (<span className='nav-link' onClick={signOutUser}>SIGN OUT</span>) : (<Link className='nav-link' to='/auth'>SIGN IN
             </Link>)}
+            <CartIcon />
         </div>
+          {isCartOpen && <CartDropdown/>}
       </div>
       <Outlet />
     </Fragment>
