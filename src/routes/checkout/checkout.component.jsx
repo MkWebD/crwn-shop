3a4feac3415749acdf34808d15772c5ca56a1265
Kevin Mas <maskevin.w@gmail.com>
2022-10-02 16:19:30 +0200

    Updating checkout component

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,34 @@
+import { useContext } from 'react'
+
+import { CartContext } from "../../contexts/cart.context";
+
+import './checkout.styles.scss'
+
+const Checkout = () => {
+    const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext)
+
+    return (
+        <div className='checkout-container'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
+            </div>
+            {cartItems.map((cartItem) => {
+                const {name, quantity, id} = cartItem
+                return <div key={id}>
+                    <h2>{name}</h2>
+                    <span>{quantity}</span>
+                    <span onClick={() => {removeItemFromCart(cartItem)}}>decrement</span>
+                    <span onClick={() => {addItemToCart(cartItem)}}>increment</span>
+                </div>
+            })}
+            <span className='total'>Total: 0</span>
+        </div>
+    );
+};
+
+export default Checkout;
