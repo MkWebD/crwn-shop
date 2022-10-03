197a9e529cd3ec9c0d0aad047428ad3c162c0ec1
Kevin Mas <maskevin.w@gmail.com>
2022-10-03 21:30:54 +0200

    Creating store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux'
 
 import App from './App'
 import {UserProvider} from "./contexts/user.context";
 import {CategoriesProvider} from "./contexts/categories.context";
 import {CartProvider} from "./contexts/cart.context";
+import {store} from './store/store'
 
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 < React.StrictMode >
+    <Provider store={store}>
   < BrowserRouter >
     < UserProvider >
       < CategoriesProvider >
@@ -20,5 +23,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       < /CategoriesProvider>
     < /UserProvider>
   < /BrowserRouter>
+</Provider>
 < /React.StrictMode>
 )
